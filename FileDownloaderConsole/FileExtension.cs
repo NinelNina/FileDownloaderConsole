7688be8b26751f1a27c29d075b08b1877a699e56
Nina <ninkr@list.ru>
2020-11-04 22:49:26 +0300

    Add class FileExtension

---
+++ FileDownloaderConsole/FileExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace FileDownloaderConsole
+{
+    class FileExtension
+    {
+        public static string GetFileExtension(string url, string fileId, string pathToSave)
+        {
+            int index = url.LastIndexOf('.');
+            string fileExtension = url.Substring(index, url.Length - index);
+            pathToSave = pathToSave + @"\" + fileId + fileExtension;
+
+            return pathToSave;
+        }
+    }
+}
