ce422b9cf189306e5f9f83c9e3be8ee0773be2d5
Nina <ninkr@list.ru>
2020-11-05 15:43:46 +0300

    Add thread sync

--- FileDownloaderConsole/FileDownloader.cs
+++ FileDownloaderConsole/FileDownloader.cs
@@ -19,11 +19,13 @@ namespace FileDownloaderConsole
         private int threadsCounter;
         private HttpClient client;
         private int degreeOfParallelism;
+        private object lockObject = new object();
         public FileDownloader()
         {
             fileDownloadQueue = new ConcurrentQueue<FileData>();
             threadsCounter = 0;
             client = new HttpClient();
+            degreeOfParallelism = 4;
         }
         struct FileData
         {
@@ -33,41 +35,48 @@ namespace FileDownloaderConsole
         }
         public void SetDegreeOfParallelism(int degreeOfParallel)
         {
-            degreeOfParallelism = degreeOfParallel;
+            if (threadsCounter != 0)
+            {
+                throw new Exception("Загрузка файлов уже запущена.");
+            }
+            else
+            {
+                degreeOfParallelism = degreeOfParallel;
+            }
         }
         public void AddFileToDownloadingQueue(string fileId, string url, string pathToSave)
         {
-            if (threadsCounter == 0)
-            {
-                SetDegreeOfParallelism(4);
-            }
-
             FileData data = new FileData();
             FileData dataForSaving;
 
             data.url = url;
-
-            int index = url.LastIndexOf('.');
-            string fileExtension = url.Substring(index, url.Length - index);
-            data.pathToSave = pathToSave + @"\" + fileId + fileExtension;
+            data.fileId = fileId;
+            data.pathToSave = FileExtension.GetFileExtension(url, fileId, pathToSave);
 
             fileDownloadQueue.Enqueue(data);
 
-            if (threadsCounter < degreeOfParallelism)
+            lock (lockObject)
             {
-                threadsCounter++;
-
-                Task dequeueTask = Task.Run(async () =>
+                if (threadsCounter < degreeOfParallelism)
                 {
-                    while (fileDownloadQueue.Count != 0)
-                    {
-                        fileDownloadQueue.TryDequeue(out dataForSaving);
-                        Console.WriteLine(dataForSaving.pathToSave);
+                    threadsCounter++;
 
-                        await DownloadFile(dataForSaving.url, dataForSaving.pathToSave);
-                    }
-                        Interlocked.Decrement(ref threadsCounter);
-                });
+                    Task dequeueTask = Task.Run(async () =>
+                    {
+                        while (fileDownloadQueue.Count != 0)
+                        {
+                            var isAddedInQueue = fileDownloadQueue.TryDequeue(out dataForSaving);
+
+                            if (isAddedInQueue)
+                            {
+                                Console.WriteLine(dataForSaving.pathToSave);
+
+                                await DownloadFile(dataForSaving.url, dataForSaving.pathToSave);
+                            }
+                        }
+                        threadsCounter--;
+                    });
+                }
             }
         }
         private async Task DownloadFile(string url, string pathToSave)
@@ -79,21 +88,20 @@ namespace FileDownloaderConsole
                     using (var content = await response.Content.ReadAsStreamAsync())
                     {
                         int fileSize = (int)content.Length;
-                        int temp = 1;
+                        int bytesRead = 1;
                         using (FileStream file = File.Create(pathToSave))
                         {
-                            while (temp != 0)
+                            while (bytesRead != 0)
                             {
                                 byte[] buf = new byte[8096];
 
-                                temp = await content.ReadAsync(buf, 0, buf.Length);
+                                bytesRead = await content.ReadAsync(buf, 0, buf.Length);
                                 await file.WriteAsync(buf, 0, buf.Length);
                             }
                         }
                     }
                 }
             }
-
         }
     }
 }
